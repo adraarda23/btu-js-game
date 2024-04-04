6d1a0a5dabdfaa7e6f99a3c5c81d9c41f978740a
adraarda23 <ardaaydinkilinc@gmail.com>
2024-04-04 04:34:39 +0300

    collision mechanic  added

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ const offset={
 const collisionMap = [];
 const boundaries = [];
 class Boundary {
-    static width = 50;
-    static height = 50;
+    static width = 64;
+    static height = 64;
     constructor({position}){
         this.position = position;
-        this.width = 50;
-        this.height = 50;
+        this.width = 64;
+        this.height = 64;
     }
     draw(){
         ctx.fillStyle = "white";
@@ -44,7 +44,7 @@ for(let i=0;i<collisionData.length;i+=140){
 collisionMap.forEach((row,i) => {
     
     row.forEach((cell,j) => {
-        if (cell !== 0)
+        if (cell == 31441)
             boundaries.push(new Boundary({
                 position: {
                     x: j*Boundary.width + offset.x,
@@ -60,8 +60,8 @@ collisionMap.forEach((row,i) => {
 
 const testBoundary = new Boundary({
     position: {
-        x: offset.x,
-        y: offset.y
+        x: offset.x +800,
+        y: offset.y +800
     }
 });
 
@@ -79,16 +79,37 @@ playerImage.src = "./images/playerDown.png";
 let lastKey = "";
 
 class Sprite{
-    constructor({position,image}){
+    constructor({position,image,frames={max:1}}){
         this.position = position;
         this.image = image;
+        this.frames = frames;
+        this.image.onload = () => {
+            this.width = this.image.width/this.frames.max;
+            this.height = this.image.height;
+        }
         
     }
     draw(){
-        ctx.drawImage(this.image, this.position.x, this.position.y,canvas.width, canvas.height);
+        ctx.drawImage(this.image, 0, 0,
+                this.image.width/this.frames.max,
+                this.image.height,
+                this.position.x,
+                this.position.y,
+             
+             this.image.width/this.frames.max,
+             this.image.height);
+             
 
     }
 }
+//playerStartX, playerStartY,
+
+const player = new Sprite({
+    position: {x: canvas.width / (2 * zoomLevel),
+         y: canvas.height / (4 * zoomLevel)},
+         image:playerImage,
+         frames: {max:4},
+});
 
 
 
@@ -116,21 +137,27 @@ const keys = {
 
     }
 
+const checkCollision = ({obj1, obj2}) => {
+    return(    obj1.position.x +obj1.width >= obj2.position.x && 
+        obj1.position.x <= obj2.position.x + obj2.width && 
+        obj1.position.y + obj1.height >= obj2.position.y &&
+        obj1.position.y <= obj2.position.y + obj2.height);
+}
 
-
-const movables = [background,testBoundary];
+const movables = [background,...boundaries];
 
 const animate = () => {
     window.requestAnimationFrame(animate);
     background.draw();
-boundaries.forEach(boundary => boundary.draw());
+boundaries.forEach(boundary => {
+    boundary.draw();
+    if(checkCollision({obj1:player, obj2:boundary})){
+        console.log("collision");
+    }
 
-    ctx.drawImage(playerImage, 0, 0,
-                playerImage.width/4,
-                 playerImage.height,
-                 playerStartX, playerStartY,
-                 playerImage.width/4,
-                 playerImage.height);
+});
+testBoundary.draw();
+    player.draw();
 
 
     if(keys.w.pressed && lastKey === "w"){
